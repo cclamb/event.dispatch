b31894f586a92dc9126e06b155bce2e0e24b0e3f
cclamb <cclamb@cs.unm.edu>
2010-09-03 22:25:25 -0600

    incrementing sequence number in remote handler

--- remote_handler.erl
+++ remote_handler.erl
@@ -1,15 +1,18 @@
 -module(remote_handler).
+-include("event_descriptor.hrl").
 -behaviour(gen_event).
 -export([init/1, handle_event/2, terminate/2, code_change/3, handle_call/2, handle_info/2]).
 
 init(_Args) ->
-    {ok, _Args}.
+    [CommandHost | _Other] = _Args,
+    {ok, [CommandHost, 1, _Other]}.
 
 handle_event(ErrorMsg, State) ->
-    [CommandHost|_] = State,
-    io:format("*** transmitting to control node *** ~p~n", [{ErrorMsg, CommandHost}]),
-	  rpc:call(CommandHost, command_server, call, [{event, ErrorMsg}]),
-    {ok, State}.
+    [CommandHost, SeqNum|_Other] = State,
+    EventMsg = ErrorMsg#event_descriptor{id = SeqNum},
+    io:format("*** transmitting to control node *** ~p~n", [{EventMsg, CommandHost, SeqNum}]),
+	  rpc:call(CommandHost, command_server, call, [{event, EventMsg}]),
+    {ok, [CommandHost, SeqNum + 1, _Other]}.
 
 terminate(_Args, _State) ->
     ok.

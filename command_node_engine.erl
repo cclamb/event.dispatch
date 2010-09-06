1761c89120d1e71feaf3f388eafd15b679440655
cclamb <cclamb@cs.unm.edu>
2010-09-06 15:15:00 -0600

    slight reorg and filename changes

--- command_node_engine.erl
+++ command_node_engine.erl
@@ -5,11 +5,12 @@
 start(_Type, StartArgs) ->
 	[CommandHost, EventManager | LinkArgs] = StartArgs,
 	io:format("*** starting application *** ~p~n", [?MODULE]),
-	Retval = client_node_supervisor:start_link(LinkArgs),
-	gen_event:add_handler(EventManager, remote_handler, [CommandHost]),
-	gen_event:add_handler(EventManager, local_handler, [CommandHost]),
-	communication_server:register(CommandHost),
-	Retval.
+  command_node_supervisor:start_link(LinkArgs).
+	%Retval = client_node_supervisor:start_link(LinkArgs),
+	%gen_event:add_handler(EventManager, remote_handler, [CommandHost]),
+	%gen_event:add_handler(EventManager, local_handler, [CommandHost]),
+	%communication_server:register(CommandHost),
+	%Retval.
 	
 stop(_State) ->
 	ok.
